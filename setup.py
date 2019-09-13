19014c568af9d8f231541944aca57eb1339d6731
Erdem Aybek <eaybek@gmail.com>
2019-09-13 22:24:29 +0300

    return old state

--- setup.py
+++ setup.py
@@ -1,42 +1,12 @@
 import setuptools
-from sna.version import VERSION
 
-# get version
-version_ = VERSION
-# increment minor version
-with open("sna/version", "w") as vf:
-    v = [int(i) for i in version_.split(".")]
-    v[2] = v[2] + 1
-    new_version = ".".join([str(i) for i in v])
-    vf.write('VERSION = "{}"'.format(new_version))
-
-# get long description
-long_description = ""
 with open("README.md", "r") as fh:
-    long_description += fh.read()
-
-
-long_description += """## Sample Usage\n"""
-long_description += """you can see the code samples\n"""
-
-# get sample usage
-with open("sna/sample.txt", "r") as fh:
-    long_description += """\n```\nsample.txt\n```\n"""
-    long_description += """```\n"""
-    long_description += fh.read()
-    long_description += """\n```"""
-
-# get sample usage
-with open("sna/sample.py", "r") as fh:
-    long_description += """\n```\nsample.py\n```\n"""
-    long_description += """\n```python3\n"""
-    long_description += fh.read()
-    long_description += """\n```\n"""
+    long_description = fh.read()
 
 
 setuptools.setup(
     name="sna",
-    version=version_,
+    version="0.0.11",
     author="Erdem Aybek",
     author_email="eaybek@gmail.com",
     description=" ".join(["Search N Act"]),
